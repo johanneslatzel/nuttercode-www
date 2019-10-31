aec290ba067681b5baa38d884a2a22afe42539c7
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2019-10-31 16:15:09 +0100

    ok response added

--- src/de/nuttercode/www/server/ResponseCode.java
+++ src/de/nuttercode/www/server/ResponseCode.java
@@ -3,7 +3,7 @@ package de.nuttercode.www.server;
 public enum ResponseCode {
 
 	ACCESS_DENIED("access denied", 403), NOT_FOUND("not found", 404),
-	INTERNAL_SERVER_ERROR("internal server error", 500), BAD_REQUEST("bad request", 400);
+	INTERNAL_SERVER_ERROR("internal server error", 500), BAD_REQUEST("bad request", 400), OK("ok", 200);
 
 	private final String message;
 	private final int code;
