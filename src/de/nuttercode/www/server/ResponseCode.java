56e64db827c89675a9b67f7977e9a70daa20b42f
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2019-11-08 20:53:36 +0100

    new code 401

--- src/de/nuttercode/www/server/ResponseCode.java
+++ src/de/nuttercode/www/server/ResponseCode.java
@@ -3,7 +3,8 @@ package de.nuttercode.www.server;
 public enum ResponseCode {
 
 	ACCESS_DENIED("access denied", 403), NOT_FOUND("not found", 404),
-	INTERNAL_SERVER_ERROR("internal server error", 500), BAD_REQUEST("bad request", 400), OK("ok", 200);
+	INTERNAL_SERVER_ERROR("internal server error", 500), BAD_REQUEST("bad request", 400), OK("ok", 200),
+	UNAUTHORIZED("unauthorized", 401);
 
 	private final String message;
 	private final int code;
