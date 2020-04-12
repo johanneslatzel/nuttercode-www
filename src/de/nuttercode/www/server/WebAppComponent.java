b704d66d628cdb5d53910de56106d49ee80f4674
Johannes B. Latzel <smartphone.johannes.latzel@outlook.de>
2020-04-12 14:00:41 +0200

    Create WebAppComponent.java

---
+++ src/de/nuttercode/www/server/WebAppComponent.java
@@ -0,0 +1,10 @@
+package de.nuttercode.www.server;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+@Documented
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface WebAppComponent {
+
+}
